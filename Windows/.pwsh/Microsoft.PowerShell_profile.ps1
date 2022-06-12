e92f6b90569a8979944aaff1df51ec0df489e1b3
Guilherme Cruz <gccruz93@gmail.com>
2022-06-11 22:15:04 -0300

    configuracao do windows terminal

---
+++ Windows/.pwsh/Microsoft.PowerShell_profile.ps1
@@ -0,0 +1,30 @@
+# $env:USERPROFILE\Documents\PowerShell
+#. $env:USERPROFILE\.config\powershell\user_profile.ps1
+
+# Alias
+Set-Alias vim nvim
+Set-Alias ll ls
+Set-Alias g git
+Set-Alias grep findstr
+Set-Alias tig 'C:\Program Files\Git\usr\bin\tig.exe'
+Set-Alias less 'C:\Program Files\Git\usr\bin\less.exe'
+
+New-Alias gt goto
+
+function goto {
+    param (
+        $location
+    )
+
+    Switch ($location) {
+        "ds" {
+            Set-Location -Path "D:/Datasigh/Projetos/www"
+        }
+        "dev" {
+            Set-Location -Path "D:/Dev"
+        }
+        default {
+            echo "Invalid location"
+        }
+    }
+}
